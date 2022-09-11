b7361430d8397944080aa4932ba03b665542057c
Krzysztof Checinski <inz.krzysztof@gmail.com>
2022-09-11 20:05:39 +0200

    Init

--- src/main/org/kester/section_8/MoreOOP_1.java
+++ src/main/org/kester/section_8/MoreOOP_1.java
@@ -6,14 +6,54 @@ package org.kester.section_8;
 //        in the middle of the word. For example: Sunday, monDay, Tuesday, wednEsday, etc.
 //        3. Print 10 randomly-chosen days of the week
 
-import java.util.Locale;
+import java.util.Random;
 
 public class MoreOOP_1 {
     public static void printDayOfWeek() {
-        System.out.println(daysOfWeek.MONDAY);
+        int numberOfElements = daysOfWeek.values().length;
+        for (int i = 0; i<numberOfElements; i++) {
+            System.out.println(daysOfWeek.values()[i]);
+        }
     }
+    public static void printDayOfWeekCapitalized() {
+        int numberOfElements = daysOfWeek.values().length;
+        for (int i = 0; i<numberOfElements; i++) {
+            String dayOfWeek = daysOfWeek.values()[i].toString().toLowerCase();
+            dayOfWeek = dayOfWeek.substring(0,1).toUpperCase() + dayOfWeek.substring(1);
+            System.out.println(dayOfWeek);
+        }
+    }
+
+    public static void printDayOfWeekMixed() {
+        int numberOfElements = daysOfWeek.values().length;
+        for (int i = 0; i<numberOfElements; i++) {
+            String dayOfWeek = daysOfWeek.values()[i].toString().toLowerCase();
+            if (i%2 == 0) {
+                dayOfWeek = dayOfWeek.substring(0, 1).toUpperCase() + dayOfWeek.substring(1);
+                System.out.println(dayOfWeek);
+            } else {
+                dayOfWeek = dayOfWeek.substring(0, dayOfWeek.length()/2) + dayOfWeek.substring(dayOfWeek.length()/2, dayOfWeek.length()/2+1).toUpperCase() + dayOfWeek.substring(dayOfWeek.length()/2+1);
+                System.out.println(dayOfWeek);
+            }
+        }
+    }
+    public static void printTenRandomDays(){
+        Random random = new Random();
+        for (int i = 0; i<10; i++) {
+            int number = random.nextInt(7);
+            System.out.printf("%s. %s\n", i+1, daysOfWeek.values()[number]);
+        }
+    }
+
+
     public static void main(String[] args) {
         printDayOfWeek();
+        System.out.println("");
+        printDayOfWeekCapitalized();
+        System.out.println("");
+        printDayOfWeekMixed();
+        System.out.println("");
+        printTenRandomDays();
     }
 
 }
