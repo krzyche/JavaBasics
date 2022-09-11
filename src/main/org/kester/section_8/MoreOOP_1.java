926382387d426d1586ecafb58331ba4c6c464aa3
Krzysztof Checinski <inz.krzysztof@gmail.com>
2022-09-11 19:24:14 +0200

    Init

---
+++ src/main/org/kester/section_8/MoreOOP_1.java
@@ -0,0 +1,10 @@
+package org.kester.section_8;
+
+//1. Use an Enum to model the days of the week and print them using a loop.
+//        1. Print them with the ﬁrst letter capitalized (without changing the Enum constants).
+//        2. Alternate between printing the ﬁrst letter capitalized or whatever letter is approximately
+//        in the middle of the word. For example: Sunday, monDay, Tuesday, wednEsday, etc.
+//        3. Print 10 randomly-chosen days of the week
+
+public class MoreOOP_1 {
+}
